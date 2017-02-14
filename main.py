49a9d2c5d51439441bf6cfacf4d6875bdfd460c9
Mahdi Hatami <iz.hatami@gmail.com>
2017-02-14 15:12:30 +0300

    done

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import tweepy
-from textblob import textblob
+from textblob import TextBlob
+import key
 
-consumer_key =
-consumer_secret =
+consumer_key = key.consumer_key
+consumer_secret = key.consumer_secret
 
-access_token =
-access_token_secret =
+access_token = key.access_token
+access_token_secret = key.access_token_secret
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.search(q='turkey', count=300)
+
+for tweet in public_tweets:
+    print(tweet.text)
+
+    # Step 4 Perform Sentiment Analysis on Tweets
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
+    print("")
